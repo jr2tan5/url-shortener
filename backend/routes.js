e694338966dac9a2766acb341a16fdb7fbb83e37
Jian Rong Tan <42073303+jr2tan5@users.noreply.github.com>
2020-09-01 04:43:40 +0800

    URL Shortener Works V1

--- backend/routes.js
+++ backend/routes.js
@@ -3,30 +3,51 @@ const baseRouteOf = require("./util/RouteUtil").baseRouteOf;
 const ResponseUtil = require("./util/ResponseUtil");
 const URLShortenerRepository = require("./repository/URLShortenerRepository");
 
+const getReqSuccess = (result) => {
+  if (result) {
+    if (!result.destination_url.startsWith("http"))
+      destinationURL = "http://" + result.destination_url;
+    else destinationURL = result.destination_url;
+    res.redirect(destinationURL);
+  } else {
+    ResponseUtil.notFound(res, "url not found");
+  }
+};
+
+const postReqSuccess = (result) => {
+  if (!result) {
+    URLShortenerRepository.saveSingleRecord(
+      db,
+      suffix,
+      destinationUrl,
+      (data) => {
+        ResponseUtil.success(res, {
+          shortenedUrl: generateShortendUrl(req, suffix),
+        });
+      },
+      errorHandler
+    );
+  } else {
+    ResponseUtil.unprocessibleEntity(res, "Suffix Already Exists");
+  }
+};
+
+const errorHandler = (error) => {
+  if (error) {
+    ResponseUtil.unprocessibleEntity(res, error);
+  }
+};
+
 const routes = (app, db) => {
   app.get(baseRouteOf("/:suffix"), (req, res) => {
     const suffix = req.params.suffix;
     URLShortenerRepository.findRecordBySuffix(
       db,
       suffix,
-      (result) => {
-        if (result) {
-          destinationURL = 'http://' + result.destination_url
-          res.redirect(destinationURL);
-        } else {
-          ResponseUtil.notFound(res, "url not found");
-        }
-      },
-      (error) => {
-        if (error) {
-          ResponseUtil.unprocessibleEntity(res, error);
-        }
-      }
+      getReqSuccess,
+      errorHandler
     );
-    // res.redirect("http://maplesea.com/");
-    // console.log();
   });
-
   app.post(apiRouteOf("/submit"), (req, res) => {
     // Check suffix contains valid characters
     const suffix = req.body.form.suffix_;
@@ -35,32 +56,8 @@ const routes = (app, db) => {
     URLShortenerRepository.findRecordBySuffix(
       db,
       suffix,
-      (result) => {
-        if (!result) {
-          URLShortenerRepository.saveSingleRecord(
-            db,
-            suffix,
-            destinationUrl,
-            (data) => {
-              ResponseUtil.success(res, {
-                shortenedUrl: generateShortendUrl(req, suffix),
-              });
-            },
-            (error) => {
-              if (error) {
-                ResponseUtil.unprocessibleEntity(res, error);
-              }
-            }
-          );
-        } else {
-          ResponseUtil.unprocessibleEntity(res, "Suffix Already Exists");
-        }
-      },
-      (error) => {
-        if (error) {
-          ResponseUtil.unprocessibleEntity(res, error);
-        }
-      }
+      postReqSuccess,
+      errorHandler
     );
   });
 };
