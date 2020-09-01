51a4179eef50e32d1f463b9cf67b7314265c9dc9
Jian Rong Tan <42073303+jr2tan5@users.noreply.github.com>
2020-09-01 16:20:35 +0800

    Added frontend build link to backend

--- backend/server.js
+++ backend/server.js
@@ -15,6 +15,14 @@ require("dotenv").config(); // Load environment variables
   );
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
+  if (process.env.NODE_ENV === 'production') {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, 'frontend/build')));
+    // Handle React routing, return all requests to React app
+    app.get('*', function(req, res) {
+      res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
+    });
+  }
   app.listen(port, () => console.log(`Listening on port ${port}`));
 
   const db = require("./MySQLConnection").connectToDB();
