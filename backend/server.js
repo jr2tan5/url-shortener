c3268d9c021ff102c524de0dd0fd7f6388e60e74
Jian Rong Tan <42073303+jr2tan5@users.noreply.github.com>
2020-09-01 22:09:09 +0800

    Fixed url redirection

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,7 @@ require("dotenv").config({
     // Serve any static files
     app.use(express.static(path.join(__dirname, "build")));
     // Handle React routing, return all requests to React app
-    app.get("/*", function (req, res) {
+    app.get("/", function (req, res) {
       res.sendFile(path.join(__dirname, "build", "index.html"));
     });
   }
