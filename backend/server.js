f0cbf71613240bebae236156991b5c3261459bec
Jian Rong Tan <42073303+jr2tan5@users.noreply.github.com>
2020-09-01 16:48:20 +0800

    Updated relative path

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 'use strict'
 const express = require("express");
 const bodyParser = require("body-parser");
+const path = require('path');
 const cors = require("cors");
 require("dotenv").config(); // Load environment variables
 
@@ -17,10 +18,10 @@ require("dotenv").config(); // Load environment variables
   app.use(bodyParser.urlencoded({ extended: true }));
   if (process.env.NODE_ENV === 'production') {
     // Serve any static files
-    app.use(express.static(path.join(__dirname, 'frontend/build')));
+    app.use(express.static(path.join(__dirname, '../frontend/build')));
     // Handle React routing, return all requests to React app
     app.get('*', function(req, res) {
-      res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
+      res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
     });
   }
   app.listen(port, () => console.log(`Listening on port ${port}`));
