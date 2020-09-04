98ed3188a56b0968a5be34e97fc0ba51350e1fbf
Jian Rong Tan <42073303+jr2tan5@users.noreply.github.com>
2020-09-05 05:06:33 +0800

    removed changes to App.tsx

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,8 +1,9 @@
+import { ThemeProvider } from "@material-ui/core";
 import "fontsource-roboto";
+import React from "react";
+import theme from "../theme/theme";
 import "./App.css";
-import React from 'react';
-import AppLayout from './AppLayout';
-import ThemeProvider from '@material-ui/core';
+import AppLayout from "./AppLayout";
 
 const App = () => {
   return (
