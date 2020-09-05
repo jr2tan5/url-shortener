bc3440539eaf9380cf296b35fbe7780e30375dde
Jian Rong Tan <42073303+jr2tan5@users.noreply.github.com>
2020-09-05 23:06:48 +0800

    Updated Path Variable LoadHtml.js

--- backend/util/loader/LoadHtml.js
+++ backend/util/loader/LoadHtml.js
@@ -6,7 +6,7 @@ const loadHtml = (express, app) => {
     app.use(express.static(path.join(__dirname, "build")));
     // Handle React routing, return all requests to React app
     app.get("/", (req, res) => {
-      res.sendFile(path.join(__dirname, "build", "index.html"));
+      res.sendFile(path.join(__dirname, "../../build", "index.html"));
     });
   }
 };
